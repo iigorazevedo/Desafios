5700b60daecdf1ce658ed3db85e6253015e95beb
Igor Azevedo <82668990+iigorazevedo@users.noreply.github.com>
2021-05-18 19:10:32 -0300

    Delete Exercício 2.py

--- Exercício 2.py
+++
@@ -1,4 +0,0 @@
-dia = input ('Insira o dia do seu nascimento:')
-mes = input ('Insira o mês do seu nascimento:')
-ano = input ('Insira o ano do seu nascimento:')
-print (f'Você nasceu no dia {dia}, no mês de {mes}, no ano de {ano}')
