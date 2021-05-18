ef2ad8996765d329141fa37bc4ac462e1a21cf52
Igor Azevedo <82668990+iigorazevedo@users.noreply.github.com>
2021-05-18 19:10:57 -0300

    Delete Exercício 3.py

--- Exercício 3.py
+++
@@ -1,4 +0,0 @@
-frase = str(input('Insira uma frase')).lower()
-print('A letra "a" aparece {} vezes na frase.'.format(frase.count('a')))
-print('A primeira letra "a" aparece na posição {}'.format(frase.find('a')+1))
-print('A última letra "a" aparece na posição {}.'.format(frase.rfind('a')+1))
